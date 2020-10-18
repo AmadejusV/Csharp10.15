9b6744e2e9e738b0a798733a3a3eba16f346742f
Amadejus Valionis <amadeus444@gmail.com>
2020-10-18 18:20:07 +0300

    nd

--- Skaitykla.MVC/Controllers/AuthController.cs
+++ Skaitykla.MVC/Controllers/AuthController.cs
@@ -17,12 +17,42 @@ namespace Skaitykla.MVC.Controllers
         [HttpPost]      //per sita formos metoda tiktai prieisim
         public IActionResult Login(LoginViewModel model)
         {
+            var People = new List<Person>()
+            {
+                new Person("Igne", "Valioniene", "igne@email.com", "password"),
+                new Person("Amadeus", "Valionis", "amadeus@email.com", "password"),
+                new Person("Vardenis", "Pavardenis", "vardenis@email.com", "password2")
+            };
+            
+
             if (!ModelState.IsValid)
             {
-                ViewBag.AuthError = "Blogi prisijungimo duomenys";
+                ViewBag.AuthError = "Nepilni prisijungimo duomenys";
                 return View("~/Views/Auth/Index.cshtml");
             }
-
+            else
+            {
+                if(People.Any(Person => Person.Email == model.Email && Person.Password == model.Password))
+                {
+                    return View("~/Views/Auth/LoggedIn.cshtml");
+                }
+                else if (People.Any(Person => Person.Email != model.Email || Person.Password != model.Password))
+                {
+                    ViewBag.AuthError = "Blogas email arba password";
+                    return View("~/Views/Auth/Index.cshtml");
+                }
+                //foreach (var person in People)
+                //{
+                //    if(person.Email==model.Email && person.Password == model.Password)
+                //    {
+                //        return View("~/Views/Auth/LoggedIn.cshtml");
+                //    }else if (person.Email != model.Email || person.Password != model.Password)
+                //    {
+                //        ViewBag.AuthError = "Blogas email arba password";
+                //        return View("~/Views/Auth/Index.cshtml");
+                //    }
+                //}
+            }
 
             return View();
         }
---
+++ Skaitykla.MVC/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Skaitykla.MVC.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public List<Person> People { get; set; }
+        public Person()
+        {
+            var People = new List<Person>();
+        }
+        public Person(string name, string surname, string email, string password)
+        {
+            Name = name;
+            Surname = surname;
+            Email = email;
+            Password = password;
+        }
+    }
+}
